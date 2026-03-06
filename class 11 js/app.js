e9d3e184a9646e917c8460705ab7f32187893032
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2026-03-06 15:44:42 +0500

    add more examples in class 11 js if else

--- class 11 js/app.js
+++ class 11 js/app.js
@@ -1,2 +1,29 @@
-let = 10;
-console.log(a);
+// if (age >= 0 && age <= 12){
+    //     console.log('Child');
+    // }
+    // else if (age >= 13 && age <= 19){
+        //     console.log('Teenager');
+// }
+// else if(age >= 20 && age <= 64){
+//     console.log('Senior');
+// }
+// else{
+    //     console.log("invalid age");
+    // }
+    
+let age = 13;
+switch (true){
+    case (age >= 0 && age <= 12):
+        console.log('child');
+        break;
+    case (age >= 13 && age <= 19):
+         console.log('teenager');
+        break;
+    case (age >= 20 && age <= 64) :
+        console.log('Senior');
+        break;
+    default:
+         console.log("invalid age");
+    
+}
+
