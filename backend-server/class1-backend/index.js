d47b93f2697518cec3795e7f61c32d954d05ce6a
mhuzaifa360 <mhuzaifa3128352877@gmail.com>
2026-04-23 16:00:48 +0500

    create all server methods in backend express js

--- backend-server/class1-backend/index.js
+++ backend-server/class1-backend/index.js
@@ -8,16 +8,41 @@ const mydata={
     course:'web development',
     id:123
 }
-app.get('/mydata', (req, res) => {
+const car ={
+  name: 'honda civic',
+  model: 2026,
+  type : 'hybrid'
+}
+// CRUD
+// create read update delete
+// post   get   put    delete
+// install npm nodemon (for automatic save changes)
+app.get('/mycar', (req, res) => {
 //   res.send(mydata)
-  res.json(mydata)
+  res.json(car)
 })
+
+
+// for create data
+app.use(express.json());
 app.post('/create', (req, res) => {
-    res.json('post called done')
+  const products = req.body;
+  const result = {
+    message : "Product create successfully",
+    data: products
+  }  
+    res.json(result )
     
 })
-app.delete('/delete', (req, res) => {
-    res.json(mydata.age)
+app.put('/updateporducts/:id',(req,res) =>{
+  const {id} = req.params;
+  console.log("my roll no is :" ,id);
+  
+  res.json(`my roll no is  = ${id}`)
+})
+
+app.delete('/deleteproducts', (req, res) => {
+    res.json('product deleted')
     
 })
 app.listen(port, () => {
